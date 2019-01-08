95ebf6a81129609b30a9e031358f18b99c0dcea9
CC <ccworld1000@gmail.com>
2019-01-08 23:18:08 +0800

    CCAutoTag push : fw

---
+++ CCGlad.framework/Versions/A/Headers/CCGlad.h
@@ -0,0 +1,19 @@
+//
+//  CCGlad.h
+//  CCGlad
+//
+//  Created by deng you hua on 2019/1/8.
+//  Copyright © 2019 CC | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CCGlad.
+FOUNDATION_EXPORT double CCGladVersionNumber;
+
+//! Project version string for CCGlad.
+FOUNDATION_EXPORT const unsigned char CCGladVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCGlad/PublicHeader.h>
+
+
