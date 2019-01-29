beb8264d3d38eec958a9c707fb62ea0ec7dc9577
CC <ccworld1000@gmail.com>
2019-01-29 18:22:52 +0800

    CCAutoTag : glad

---
+++ CCGladCMD/CCGladCMD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CCGladCMD
+//
+//  Created by dengyouhua on 2019/1/29.
+//  Copyright © 2019 cc | ccworld1000@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CCGlad/glad.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CCGlad
+
+CCGlad glad for mac
+
+
+
+## Support Pod for mac
+
+```shell
+pod 'CCGlad'
+```
+
